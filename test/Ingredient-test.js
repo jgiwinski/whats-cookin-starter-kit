c6035a320a8d7e0d88175ba02c1f461406673e1e
Marika <marikashanahan@gmail.com>
2021-02-07 11:38:22 -0700

    Create Ingredient.js and Ingredient-test.js files

---
+++ test/Ingredient-test.js
@@ -0,0 +1,22 @@
+const chai = require('chai')
+const expect = chai.expect
+const Ingredient = require('../src/Ingredient')
+const dummyIng = [{
+  "id": 15389,
+  "name": "rolled oats",
+  "estimatedCostInCents": 112
+},
+{
+  "id": 536,
+  "name": "sparkling water",
+  "estimatedCostInCents": 236
+},
+{
+  "id": 1123964,
+  "name": "baking chocolate",
+  "estimatedCostInCents": 785
+}]
+
+describe('Ingredient', () => {
+  it('should')
+})
