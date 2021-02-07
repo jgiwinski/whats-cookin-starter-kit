acbf412b04184c49516a9f96f231ac6b2460f768
Marika <marikashanahan@gmail.com>
2021-02-07 12:35:54 -0700

    Create Recipe.js and Recipe-test.js

---
+++ test/Recipe-test.js
@@ -0,0 +1,8 @@
+const chai = require('chai')
+const expect = chai.expect
+const Recipe = require('../src/Recipe')
+const Ingredient = require('../src/Ingredient')
+
+describe('Recipe', () => {
+  it()
+})
