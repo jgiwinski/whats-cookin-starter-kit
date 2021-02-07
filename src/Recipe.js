0d62f31ebf2eb6d577270ac34326e84a829d746f
Marika <marikashanahan@gmail.com>
2021-02-07 15:19:22 -0700

    Write first draft Recipe-test.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,3 +5,8 @@ class Recipe {
 }
 
 module.exports = Recipe;
+
+// It should have methods to:
+// * Determine the names of ingredients needed
+// * Get the cost of its ingredients
+// * Return its directions / instructions
